2e0b82b037101e92d9d0951fea8f0c1f424c542b
Aslbek0730 <aslbekakhmedov1@gmail.com>
2023-11-21 00:50:58 +0500

    profile

--- users/tests.py
+++ users/tests.py
@@ -1,4 +1,21 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from .models import CustomUser
 # Create your tests here.
 
+class SignupTestCase():
+    def test_signup_view(self):
+        response = self.client.post(
+            reverse('users:signup'),
+            data = {
+                'firstname':'Aslbek',
+                'username':'Aslbek',
+                'email':' ',
+                'password1':'qweqweqwe',
+                'password2':'qweqweqwe',
+            }
+        )
+        user=CustomUser.objects.get(username='Aslbek')
+        self.assertEqual(user.first_name, 'Aslbek')
+        self.assertEqual(user.email, ' ')
+        self.assertTrue(user.check_password('qweqweqew'))
