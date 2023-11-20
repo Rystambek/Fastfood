0ec778acdd1cf0432baffbc71b27170d319fe458
Aslbek0730 <aslbekakhmedov1@gmail.com>
2023-11-21 00:21:12 +0500

    bolaverdi

--- Fastfood/settings.py
+++ Fastfood/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'foods',
     'main',
     'users',
@@ -124,6 +126,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath("static")),)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
