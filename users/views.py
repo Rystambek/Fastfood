8631e299e14190fc38d6a4af623f02239feea4bb
Aslbek0730 <aslbekakhmedov1@gmail.com>
2023-11-21 02:09:22 +0500

    yarmidaman

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.views import View
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 class SignupView(View):
     def get(self, request):
@@ -14,3 +16,8 @@ class SignupView(View):
             messages.success(request, 'Your account is successfully created.')
             return redirect('login')
         return render(request, 'registration/signup.html', {'form': form})
+    
+class ProfileView(View):
+    def get(self, request, username):
+        user = get_object_or_404(CustomUser, username=username)
+        return render(request, 'profile.html', {'customuser':user})
